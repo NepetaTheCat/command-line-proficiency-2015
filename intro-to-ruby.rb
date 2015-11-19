b585b62736de2f8ccf7a421c73479c6252a93574
nepetathecat <amaryceosorio@gmail.com>
2015-11-19 14:52:47 +0000

    meep

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -4,9 +4,9 @@ puts "#{name}"
 #print out the value of the variable you just created
 puts name.length
 #print the sum of 185 and 14
-print 185 + 14
+puts 185 + 14
 #print the product of 1.8 and 3.6
-print 1.8 * 3.6
+puts 1.8 * 3.6
 #store the number 78 as a variable
 number = 78
 #print the average of the following numbers: 78, 55, 63, 98
